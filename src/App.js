dc77428a0de961b1605ea0ec8bb4bb6c2bb7d7c2
TudorBucur19 <tudor.bucur88@gmail.com>
2020-11-20 12:17:16 +0200

    add css classes, dice 1 condition

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from 'react';
 import './App.css';
 import { render } from '@testing-library/react';
 import {Player} from './components/player.jsx';
-//import {RollDice} from './components/rollDiceButton.jsx';
-
 
 
 class App extends React.Component {
@@ -12,20 +10,19 @@ class App extends React.Component {
   state = {
     activePlayer: 0,
     scores: [0, 0],
-    // score1: 0,
-    // score2: 0,
     currentScore: [0, 0],
-    //currentScore2: 0,
     dice: 0
    }
 
   rollDice = () => {    
       const {activePlayer, currentScore} = this.state;
       let currentDice = Math.trunc(Math.random()*6)+1;
-      //let currentScore = this.state.currentScore;
-      currentScore[activePlayer] += currentDice;
-      console.log(currentDice);      
-      this.setState({dice: currentDice, currentScore: currentScore})
+      if(currentDice !== 1){
+        currentScore[activePlayer] += currentDice;            
+        this.setState({dice: currentDice, currentScore: currentScore})
+      } else {
+        this.switchPlayer();
+      }
     };
 
   switchPlayer = () => {
@@ -33,7 +30,6 @@ class App extends React.Component {
     active = active === 0 ?  1: 0;
     this.setState({activePlayer: active});
     this.setState({currentScore: [0, 0]});
-    // de adaugat sa modifice si clasa de css: active
   }
 
   btnHold = () => { 
@@ -52,20 +48,22 @@ class App extends React.Component {
   return (
     <div className="App">
       <main className = "main">
-        <img src={`../images/dice-${this.state.dice}.png`} alt="Playing dice" className="dice" />
+        <img src={`../images/dice-${this.state.dice}.png`} 
+        alt="Playing dice" 
+        className={this.state.dice === 0 ? "dice hidden" : "dice"} />
         <button className="btn btn--new">🔄 New game</button>
         <button className="btn btn--roll" onClick={this.rollDice}>🎲Roll dice</button>
         <button className="btn btn--hold" onClick={this.btnHold}>📥 Hold</button>
         <Player 
           score = {this.state.scores[0]}
           currentScore = {this.state.currentScore[0]}
-          className = "player  player--active"
+          className ={this.state.activePlayer === 0 ? "player  player--active" : "player"}
           id = "0"
         />
         <Player 
           score = {this.state.scores[1]}
           currentScore = {this.state.currentScore[1]}
-          className = "player"
+          className = {this.state.activePlayer === 0 ? "player" : "player player--active"}
           id = "1"
         />
       </main>
